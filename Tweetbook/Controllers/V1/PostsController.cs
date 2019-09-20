0052aa56ef044a568c3a7c383c7f15dd6e9c5695
NXNote5130 <nickrhollenbeck@gmail.com>
2019-09-19 21:07:05 -0700

    Initial commit

---
+++ Tweetbook/Controllers/V1/PostsController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Contracts;
+using Tweetbook.Controllers.V1.Requests;
+using Tweetbook.Controllers.V1.Responses;
+using Tweetbook.Domain;
+using Tweetbook.Services;
+
+namespace Tweetbook.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly IPostService _postService;
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var posts = await _postService.GetPostAsync();
+            return Ok(posts);
+        }
+
+        [HttpGet(ApiRoutes.Posts.Get)]
+        public async Task<IActionResult> Get([FromRoute] Guid postId)
+        {
+            var post = await _postService.GetPostByIdAsync(postId);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+        [HttpPut(ApiRoutes.Posts.Update)]
+        public async Task<IActionResult> Update([FromRoute] Guid postId, [FromBody] UpdatePostRequest postRequest)
+        {
+            var post = new Post
+            {
+                Id = postId,
+                Name = postRequest.Name
+            };
+
+            var updated = await _postService.UpdatePostAsync(post);
+
+            if (!updated)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public async Task<IActionResult> Create([FromBody] CreatePostRequest postRequest)
+        {
+            var post = new Post { Name = postRequest.Name };
+
+            await _postService.CreatePostAsync(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
+
+            var postResponse = new PostResponse { Id = post.Id };
+            return Created(locationUri, postResponse);
+        }
+
+      [HttpDelete(ApiRoutes.Posts.Delete)]
+      public async Task<IActionResult> Delete([FromRoute] Guid postId)
+        {
+            var isDeleted = await _postService.DeletePostAsync(postId);
+
+            if (!isDeleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Tweetbook/Services/PostService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetbook.Data;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly DataContext _dataContext;
+        
+        public PostService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<bool> DeletePostAsync(Guid postId)
+        {
+            var post = await GetPostByIdAsync(postId);
+            _dataContext.Posts.Remove(post);
+            await _dataContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Post>> GetPostAsync()
+        {
+            return await _dataContext.Posts.ToListAsync();
+        }
+
+        public async Task<Post> GetPostByIdAsync(Guid postId)
+        {
+            return await _dataContext.Posts.SingleOrDefaultAsync(p => p.Id == postId);
+        }
+
+        public async Task<bool> UpdatePostAsync(Post postToUpdate)
+        {
+            _dataContext.Posts.Update(postToUpdate);
+            var updated = await _dataContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> CreatePostAsync(Post post)
+        {
+            await _dataContext.Posts.AddAsync(post);
+            var created = await _dataContext.SaveChangesAsync();
+
+            return created > 0;
+        }
+    }
+}
